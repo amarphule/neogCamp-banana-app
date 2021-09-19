d9423a28bbc19ca1af6119e98887d1af8c69f4eb
Amar Phule <amardeepphule@gmail.com>
2021-09-19 17:14:31 +0530

    feat: final output

--- app.js
+++ app.js
@@ -3,19 +3,28 @@ const inputArea = document.querySelector('#input-text')
 const outputArea = document.querySelector('.output')
 
 
-button.addEventListener('click', () => {
-    const url = 'https://api.funtranslations.com/translate/ubbi-dubbi.json?text=' + inputArea.value
-    fetch(url)
-        .then(
-            res => res.json()
-        )
-        .then(
-            json => {
-                outputArea.innerHTML = json.contents.translated
-            }
-        )
-        .catch(() => {
-            alert('Error Occured')
-        }
-        )
-})
+var btntranslate = document.querySelector('.btn-translate')
+var textInput = document.querySelector("#input-text")
+var textoutput = document.querySelector(".output")
+var serverurl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationUrl(text) {
+    return serverurl + "?text=" + text
+}
+
+function errorhandler(error) {
+    console.log("Error Occured", error)
+}
+
+function clickHandler() {
+    var inputText = textInput.value;
+    fetch(getTranslationUrl(inputText))
+        .then(respnose => respnose.json())
+        .then(json => {
+            var translatedText = json.contents.translated
+            textoutput.innerText = translatedText
+        })
+        .catch(errorhandler)
+}
+
+btntranslate.addEventListener("click", clickHandler)
